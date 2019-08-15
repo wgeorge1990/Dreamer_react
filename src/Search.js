acf2d04a2384253be664348d14fdd791818b7cb0
wgeorge1990 <williameverettgeorge@icloud.com>
2019-08-15 16:26:51 -0400

    restored to stable working version

--- src/Search.js
+++ src/Search.js
@@ -1,20 +1,54 @@
 import React, { Component } from 'react';
 import Unsplash from 'unsplash-js';
+import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button'
 
 
 class Search extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            photos: [],
+            photoUrls: []
+        }
+    }
 
-    unsplash = new Unsplash({
-            applicationId: "{APP_ACCESS_KEY}",
-            secret: "{APP_SECRET}"
-    });
-    
+    componentDidMount() {
+         let unsplash = new Unsplash({
+             applicationId: process.env.REACT_APP_ACCESS_KEY,
+             secret: process.env.REACT_APP_SECRET
+         });
+         unsplash.photos.listPhotos(2, 5, "latest")
+             .then(data => data.json())
+             .then(json => {
+                 this.setState({
+                     photos: json
+                 })
+             })
+         
+    }
+
     render() {
-        console.log(this.unsplash)
-        console.log(process.env.REACT_APP_TEST_SECRET)
+        console.log(Carousel.props)
         return (
-            <div className="Search">
-               Search
+            <div className="Search container grid">
+                <Carousel  className="row">
+                    {this.state.photos.map(photo => {
+                        return (
+                            < Carousel.Item key={photo.id} >
+                                <img 
+                                    className="d-block w-100 "
+                                    src={`${photo.urls.small}`}
+                                    alt={photo.id}
+                                />
+                                <Carousel.Caption>
+                                    <Button>Select</Button>
+                                <h3>{photo.Caption}</h3>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        )
+                    })}
+                </Carousel>
             </div>
         )
     }

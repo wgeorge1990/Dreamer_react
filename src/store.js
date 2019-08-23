fe9791384343623427fcb05ecfef5219d1a0576f
wgeorge1990 <williameverettgeorge@icloud.com>
2019-08-23 18:59:38 -0400

    Redux integration complete

--- src/store.js
+++ src/store.js
@@ -1,8 +1,13 @@
-import { createStore} from 'redux'
+import { createStore } from 'redux'
+import { composeWithDevTools, devToolsEnhancer } from 'redux-devtools-extension'
 import rootReducer from './reducers/root'
 
-const store = createStore(
-    rootReducer
-)
+// const store = createStore(
+//     rootReducer
+// )
+
+const store = createStore(rootReducer, /* preloadedState, */ devToolsEnhancer(
+    // Specify custom devTools options
+));
 
 export default store

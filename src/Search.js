614104f13ca9df1e20c623e040bd12e37a7c11b3
wgeorge1990 <williameverettgeorge@icloud.com>
2019-08-26 17:54:28 -0400

    Adjusted formating of responsive grid

--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Grid, Image, Input, Form, Button, Container } from 'semantic-ui-react'
 import { connect } from 'react-redux';
-import {unsplashRequest} from './services/requests'
+import { unsplashRequest } from './services/requests'
 
 class Search extends Component {
     constructor(props) {
@@ -11,7 +11,7 @@ class Search extends Component {
     componentDidMount() {
         //unsplashRequest is a namedimport from services/requests
         unsplashRequest().then(json => {
-            this.props.dispatch({type: 'FETCHPHOTOS', payload: json.results})
+            this.props.dispatch({ type: 'FETCHPHOTOS', payload: json.results })
         }).then(this.splitCollectionForGrid)
     }
 
@@ -19,8 +19,8 @@ class Search extends Component {
         let halfWayThrough = Math.floor(this.props.images.photos.length / 2)
         let arrayFirstHalf = this.props.images.photos.slice(0, halfWayThrough);
         let arraySecondHalf = this.props.images.photos.slice(halfWayThrough, this.props.images.photos.length);
-        this.props.dispatch({type: 'SETIMAGECONTAINERONE', payload: arrayFirstHalf})
-        this.props.dispatch({type: 'SETIMAGECONTAINERTWO', payload: arraySecondHalf})
+        this.props.dispatch({ type: 'SETIMAGECONTAINERONE', payload: arrayFirstHalf })
+        this.props.dispatch({ type: 'SETIMAGECONTAINERTWO', payload: arraySecondHalf })
         // this.setState({ first: arrayFirstHalf, second: arraySecondHalf})
     }
 
@@ -38,65 +38,58 @@ class Search extends Component {
     render() {
         // let selection = this.state.photos.filter(photo => Number(photo.height) > Number(photo.width))
         return (<div>
-            
-                <Form className="form" onSubmit={this.onSearchChange} >
-                   {/* <Container> */}
-                    <Input
-                        fluid
-                        name="searchTerm"
-                        icon='search'
-                        placeholder="Search Term"
-                        // style={{'width' : "31rem"}}
-                    />
+
+            <Form className="form" onSubmit={this.onSearchChange} >
+                {/* <Container> */}
+                <Input
+                    fluid
+                    name="searchTerm"
+                    icon='search'
+                    placeholder="Search Term"
+                // style={{'width' : "31rem"}}
+                />
                 <Button
-                            id='addButton'
-                            fluid
-                            style={{ 'marginTop': '6px', 'marginBottom': '8px'}}
-                            type='submit'
-                        > 
-                            Search
+                    id='addButton'
+                    fluid
+                    style={{ 'marginTop': '6px', 'marginBottom': '8px' }}
+                    type='submit'
+                >
+                    Search
                         </Button>
-                   {/* </Container> */}
-                    </Form>
-            
-        
+                {/* </Container> */}
+            </Form>
             <Grid columns={2} divided>
                 <Grid.Row>
-                <Grid.Column>
-                <Grid container columns={1}>
-                    {this.props.images.imageContainerOne.map(photo => 
-                        <Grid.Column key={Math.random()}>
-                            <Container>
-                                <Image
-                                    onClick={(e) => this.props.showDetail(e, photo, photo.urls.regular)}
-                                    src={photo.urls.regular} fluid />
-                                Photo by <a href={`https://unsplash.com/@${photo.user.username}?utm_source=your_app_name&utm_medium=referral`}>{photo.user.first_name + ' ' + photo.user.last_name}</a> on <a href="https://unsplash.com/?utm_source=your_app_name&utm_medium=referral">Unsplash</a>
-                            </Container>
-                    </Grid.Column>)}
-                    </Grid>
-
+                    <Grid.Column>
+                        <Grid container columns={1}>
+                            {this.props.images.imageContainerOne.map(photo =>
+                                <Grid.Column key={Math.random()}>
+                                    <Container>
+                                        <Image
+                                            onClick={(e) => this.props.showDetail(e, photo, photo.urls.regular)}
+                                            src={photo.urls.regular} fluid />
+                                        Photo by <a href={`https://unsplash.com/@${photo.user.username}?utm_source=your_app_name&utm_medium=referral`}>{photo.user.first_name + ' ' + photo.user.last_name}</a> on <a href="https://unsplash.com/?utm_source=your_app_name&utm_medium=referral">Unsplash</a>
+                                    </Container>
+                                </Grid.Column>)}
+                        </Grid>
                     </Grid.Column>
-                    
                     <Grid.Column>
                         <Grid columns={1}>
-                            
-                                {this.props.images.imageContainerTwo.map(photo => 
-                                < Grid.Column key = {
+                            {this.props.images.imageContainerTwo.map(photo =>
+                                < Grid.Column key={
                                     Math.random()
                                 } >
-                            <       Container>
+                                    <Container>
                                         <Image
                                             src={photo.urls.regular} fluid
                                             onClick={(e) => this.props.showDetail(e, photo, photo.urls.regular)}
-                                            />
+                                        />
                                         Photo by <a href={`https://unsplash.com/@${photo.user.username}?utm_source=your_app_name&utm_medium=referral`}>{photo.user.first_name + ' ' + photo.user.last_name}</a> on <a href="https://unsplash.com/?utm_source=your_app_name&utm_medium=referral">Unsplash</a>
-
                                     </Container>
-                                    </Grid.Column>)}
-                            
+                                </Grid.Column>)}
                         </Grid>
                     </Grid.Column>
-                    </Grid.Row >
+                </Grid.Row >
             </Grid>
         </div>
         )
